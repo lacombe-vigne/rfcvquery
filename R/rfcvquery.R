e1ab8842b82117f16406b7f5551376c510232b5c
Timothée Flutre <timothee.flutre@supagro.inra.fr>
2016-10-06 17:15:53 +0200

    change fct names + improv man and README

--- R/rfcvquery.R
+++ R/rfcvquery.R
@@ -1,47 +1,59 @@
-##' Return introduction names.
+##' Introduction/accession level
 ##'
-##' http://bioweb.ensam.inra.fr/collections_vigne/Intro.php
+##' Return the introduction/accession names from a set of introduction/accession codes.
 ##' @param conn a DBIConnection object, as produced by dbConnect
 ##' @param intro.codes vector of introduction codes ("CodeIntro")
 ##' @return vector of introduction names
 ##' @author Timothée Flutre
 ##' @export
-get.intro.name <- function(conn, intro.codes){
-  stopifnot(is.vector(intro.codes))
+getIntroName <- function(conn, intro.codes){
+  stopifnot(is.vector(intro.codes),
+            all(! duplicated(intro.codes)))
+
   intro.names <- sapply(intro.codes, function(intro.code){
-    query <- paste0("SELECT NomIntro FROM `NV-INTRODUCTIONS` WHERE CodeIntro=\"",
+    query <- paste0("SELECT NomIntro",
+                    " FROM `NV-INTRODUCTIONS`",
+                    " WHERE CodeIntro=\"",
                     intro.code, "\"")
-    res <- DBI::dbSendQuery(conn, query)
-    tmp <- DBI::dbFetch(res)
+    res <- dbSendQuery(conn, query)
+    tmp <- dbFetch(res)
     tmp$NomIntro
   })
+
   return(intro.names)
 }
 
-##' Return variety codes and names.
+##' Variety/Population level
 ##'
-##' http://bioweb.ensam.inra.fr/collections_vigne/Variete.php
+##' Return the variety/population codes and names from a set of introduction/accession codes.
 ##' @param conn a DBIConnection object, as produced by dbConnect
 ##' @param intro.codes vector of introduction codes ("CodeIntro")
-##' @return vector of variety names
+##' @return data.frame of variety codes and names
 ##' @author Timothée Flutre
 ##' @export
-get.variety.code.name <- function(conn, intro.codes){
+getVarietyCodeName <- function(conn, intro.codes){
   stopifnot(is.vector(intro.codes))
+
   variety.codes <- sapply(intro.codes, function(intro.code){
-    query <- paste0("SELECT CodeVar FROM `NV-INTRODUCTIONS` WHERE CodeIntro=\"",
+    query <- paste0("SELECT CodeVar",
+                    " FROM `NV-INTRODUCTIONS`",
+                    " WHERE CodeIntro=\"",
                     intro.code, "\"")
-    res <- DBI::dbSendQuery(conn, query)
-    tmp <- DBI::dbFetch(res)
+    res <- dbSendQuery(conn, query)
+    tmp <- dbFetch(res)
     tmp$CodeVar
   })
+
   variety.names <- sapply(variety.codes, function(variety.code){
-    query <- paste0("SELECT NomVar FROM `NV-VARIETES` WHERE CodeVar=\"",
+    query <- paste0("SELECT NomVar",
+                    " FROM `NV-VARIETES`",
+                    " WHERE CodeVar=\"",
                     variety.code, "\"")
-    res <- DBI::dbSendQuery(conn, query)
-    tmp <- DBI::dbFetch(res)
+    res <- dbSendQuery(conn, query)
+    tmp <- dbFetch(res)
     tmp$NomVar
   })
+
   return(data.frame(CodeVar=variety.codes,
                     NomVar=variety.names))
 }
--- README.md
+++ README.md
@@ -1,13 +1,42 @@
-rfcvquery
-=========
+Package "rfcvquery"
+==================
 
-This directory contains code of the R package named "rfcvquery". It is funded
-by the Institut National de la Recherche Agronomique (INRA). The coordinators
-are Timothée Flutre and Thierry Lacombe.
+This directory contains the "rfcvquery" package for the R programming language.
+This package helps querying the database of the collections from the French Network of Grapevine Repositories ([RFCV](https://bioweb.supagro.inra.fr/collections_vigne/Home.php?l=EN)).
+The development is funded by the Institut National de la Recherche Agronomique ([INRA](https://www.inra.fr/)).
+The coordinators are Timothée Flutre and Thierry Lacombe.
 
-The copyright is owned by the Institut National de la Recherche Agronomique.
-The code is available under the GPL-3, see the file LICENSE.
+The copyright is owned by the INRA.
+The code is available under the GNU Public License (version 3 and later).
+See the COPYING file for usage permissions.
 
-The content of this directory is versioned using git, the central repository
-being hosted on GitHub:
-https://github.com/lacombe-vigne/rfcvquery
+The content of this directory is versioned using git, the central repository being hosted on [GitHub](https://github.com/lacombe-vigne/rfcvquery).
+You can report issues on GitHub directly, but remember to copy-paste the output of sessionInfo().
+
+**For users**, the easiest is to directly install the package from GitHub:
+```
+R> library(devtools); install_github("lacombe-vigne/rfcvquery")
+```
+
+In the case where the package is officially released as a "bundled package" (that is, as a tar.gz), you can install it directly from within R via:
+```
+R> install.packages("path/to/rfcvquery_<version>.tar.gz")
+```
+
+**For advanced users**, when retrieving the bundled package, you can install it from the command-line via:
+```
+$ R CMD INSTALL rfcvquery_<version>.tar.gz
+```
+
+Once the package is installed, start using it:
+```
+R> library(rfcvquery)
+R> help(package=rfcvquery)
+```
+
+**For developpers**, when editing the content of this repo, increment the version of the package in DESCRIPTION and execute the following commands:
+```
+$ Rscript -e 'library(devtools); devtools::document()'
+$ R CMD build rfcvquery
+$ R CMD check rfcvquery_<version>.tar.gz
+```
